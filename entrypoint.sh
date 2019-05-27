d683f4255ae502354c899912c811b59f6ba8d16f
shinhwagk <shanghai_sl@hotmail.com>
2019-05-27 18:13:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -e
 
+if [ -z "$REMOTE_BASH_URL" ]; then
+  echo "\$REMOTE_BASH_URL" not found
+  exit 1
+fi
+
+if [ -z "$COMMAND_NAME" ]; then
+  echo "\$COMMAND_NAME" not found
+  exit 1
+fi
+
 curl -o /usr/local/bin/$COMMAND_NAME $REMOTE_BASH_URL
 
 /usr/local/bin/$COMMAND_NAME
