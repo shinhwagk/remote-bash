ea99a3a23fe8d7b3beabe3e56e81bc04d1436fad
shinhwagk <shanghai_sl@hotmail.com>
2019-05-27 18:36:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,4 @@ if [ -z "$REMOTE_BASH_URL" ]; then
   exit 1
 fi
 
-if [ -z "$COMMAND_NAME" ]; then
-  COMMAND=${COMMAND_NAME:-${REMOTE_BASH_URL##*/}}
-fi
-
-curl -o /usr/local/bin/$COMMAND $REMOTE_BASH_URL
-chmod +x /usr/local/bin/$COMMAND
-
-/usr/local/bin/$COMMAND $@
+ curl -sLk $REMOTE_BASH_URL | bash -s -- $@
